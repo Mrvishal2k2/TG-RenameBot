377fd490e1d0aa7ea6b7bbd0c2cd1ad87ccf8cdd
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-05-23 01:02:42 +0530

    Update config.py

--- root/config.py
+++ root/config.py
@@ -7,7 +7,7 @@ Dont kang !!!
 '''
 import os 
 
-class Config (Object):
+class Config(object):
   APP_ID = int(os.environ.get("APP_ID", ""))
   API_HASH = os.environ.get("API_HASH", "")
   BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
