b19568fa618a9609815f292527c5a0a7397cfa1d
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-05-29 16:34:59 +0530

    Update uploader.py

--- root/utils/uploader.py
+++ root/utils/uploader.py
@@ -32,9 +32,9 @@ async def uploader(bot,file, update, msg,as_file=False):
 
     start_time = time.time() 
     afilename = file.split("/")[-1]
-    filename = afilename.split(".",1)[0:-1]
+    filename = " ".join(afilename.split(".")[0:-1])
     if Config.CUSTOM_CAPTION:
-         filename += f"\n{Config.CUSTOM_CAPTION}"
+         filename = filename + "\n" + Config.CUSTOM_CAPTION
     # Thumb Location parameter 
     thumb_location = Config.DOWNLOAD_LOCATION + "/thumb/" + str(update.chat.id) + ".jpg" 
     thumb_image_path = None
