c91f354f17e404f428d4954192fd61e29fafdc0d
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2021-05-29 15:28:46 +0530

    Update messages.py

--- root/messages.py
+++ root/messages.py
@@ -1,7 +1,7 @@
 class Translation(object):
     START_TEXT = "**I'm a Rename and Convert Bot\nJust send me any media to change file name.\nUse /help command for more details **"
 ######################
-    HELP_USER = """**Just send me a media to change filename \n>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
+    HELP_USER = """**>>Send File/Video\n>>Select desired Option\n>>And Done wait for it to process files**"""
     DOWNLOAD_MSG = "**Downloading **⏬"
     DOWNLOAD_FAIL_MSG = "**Failed to Download File**❎"
     UPLOAD_MSG = "**Uploading** ⏫"
