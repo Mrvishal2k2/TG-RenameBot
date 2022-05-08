2e5e086676461f8c225f001cdf2035cca4132e9d
Mr Vishal <67860986+Mrvishal2k2@users.noreply.github.com>
2022-05-09 01:16:50 +0530

    Update database.py

--- root/utils/database.py
+++ root/utils/database.py
@@ -2,14 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-import os
-import threading
-import asyncio
+import os, asyncio, threading
 from sqlalchemy import Column, Integer, Boolean, String, ForeignKey, UniqueConstraint, func
-
-
 from root.config import Config
-
 
 def start() -> scoped_session:
     engine = create_engine(Config.DB_URI, client_encoding="utf8")
